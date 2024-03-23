86b92c27adca91d09cb4b57b4602ff6d6304ffd0
azizou98 <abdelaziz.aitmoussa@etu.usthb.dz>
2024-03-23 16:16:49 +0100

    my 21/03 changes

--- myApp.js
+++ myApp.js
@@ -1,10 +1,54 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 console.log('Hello World')
 
+absolute= __dirname + '/views/index.html';
+
+public_path= __dirname+ '/public'
+app.use('/public',express.static(public_path));
+
+app.use((req,res,next)=>{
+  console.log(req.method +" "+req.path+" "+"-"+" "+req.ip );
+  next();
+})
+
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();  // operation
+  next();
+}, function(req, res) {
+  res.json({"time":req.time});
+});
+
+app.get('/',function(req, res) {
+    res.sendFile(absolute)
+  })
 
 
+app.get('/json',function(req, res) {
+
+  if(process.env.MESSAGE_STYLE=='uppercase')
+{
+  res.json({"message":"Hello json".toUpperCase()})
+}else{
+  res.json({"message":"Hello json"})
+}
+});
+
+app.get('/:word/echo',(req,res)=>{
+
+  res.json({'echo':req.params.word}) // meme lokan ndirha echo :
+                                       // temchi bla hado ''
+})
+
+app.route('/name').get((req,res)=>{
+  res.json(
+    {
+      name: req.query.first + ' ' + req.query.last 
+    }
+  )
+})
 
 
 
